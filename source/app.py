4a1bfa8bd736ef3583c6ed7e96676196c7f0946f
Duncan Mazza <dmazza@olin.edu>
2019-04-20 22:50:17 -0400

    Add capability to show multiple cameras in web app

--- source/app.py
+++ source/app.py
@@ -2,8 +2,11 @@
 Web application app
 """
 
-from flask import render_template
+from flask import render_template, Response, request, session, redirect, jsonify
 from web_classes import WebApplication
+from cv_classes import ProcessingEngine
+import time
+
 
 
 def index(error=False):
@@ -13,21 +16,55 @@ def index(error=False):
 
 
 def select_feeds(error=False):
-    return render_template('select_feeds.html', visibility=("visible" if error else "hidden"))
+    return render_template('select_feeds.html', NUM_CAPS=engine.num_caps-1)
+
 
 def more_info(error=False):
     return render_template('more_info.html', visibility=("visible" if error else "hidden"))
 
 
+def feed(engine, cap_num):
+    """
+    Opens a camera reader, gets a processed frame, encodes it to JPEG, and returns it as a
+    snippet of a multipart response body.
+        Author of this function: Elias Gabriel
+    """
+    # In a loop, get the current frame of the camera as a byte sequence and yield it to the calling process.
+    # This lets us send a HTTP response back to the client, but keeps it open to allow for continious
+    # updates. In effect, this streams image data from the server to the client's computer through a
+    # Motion JPEG.
+    # Wrap the encoded frame in a multipart image section, to be inserted into the multipart HTTP response
+    while True:
+        yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + engine.get_frame(cap_num) + b'\r\n')
+
+
+def eye(CAP_NUM):
+    """ Returns a mixed multipart HTTP response containing streamed MJPEG data, pulled from
+    the OpenCV image processor. """
+    # if int(time.time()) % 3 == 0:
+    #     engine.refresh()
+
+    # Create and return a mutlipart HTTP response, with the separate parts defined by '--frame'
+    try:
+        return Response(feed(engine, CAP_NUM), mimetype='multipart/x-mixed-replace; boundary=frame')
+    except:
+        return index(error=True)
+
+# Create a processing engine. Although it generally isn't good practice to do this in the body of the document, the
+# object needs to be a global instance so that only one is created no matter how many cameras are created.
+engine = ProcessingEngine()
+
 if __name__ == "__main__":
+
     # Create a new web application
     app = WebApplication("pedheatmap")
+
     # Define the application routes
     app.route({
         '/': index,
         '/select_feeds': select_feeds,
         '/more_info': more_info,
-    })
+        '/<CAP_NUM>': eye})
 
     # Beginning listening on `localhost`, port 3000
-    app.listen(port=8080, env="development")
+    app.listen(port=8080)
--- source/web_classes.py
+++ source/web_classes.py
@@ -4,13 +4,27 @@ Contains classes used for the web application.
 For the final project by Duncan, Michael, Nick, and Gabriella of the Software Design class at Olin College of
 Engineering in the spring of 2019.
 
-@Author: Elias Gabriel (used with permission for this project; some modifications were made)
+@Author: originally Elias Gabriel (used with permission for this project; modifications were made by Duncan Mazza)
 """
-
 
 from flask import Flask
 import os
 from flask_session import Session
+from redis import Redis, ConnectionError
+from cv_classes import ProcessingEngine
+
+
+def launch_redis():
+    """
+    Launches the redis server
+    :return: rs - Redis object
+    """
+    rs = Redis('localhost')
+    try:
+        rs.ping()
+        return rs
+    except ConnectionError:
+        return False
 
 
 class WebApplication(Flask):
@@ -25,14 +39,21 @@ class WebApplication(Flask):
         # Set configuration variables
         self.debug = debug
         self.config['SECRET_KEY'] = os.urandom(16)
+        self.config['SESSION_REDIS'] = launch_redis()
         Session(self)  # for the cookies
 
     def listen(self, **options):
         """ Asks Flask to begin listening to HTTP requests, with options if given. """
-        # If a host is not given, assume localhost
-        self.run(options.get('host', "127.0.0.1"), options.get('port', 3000), options)
+        # If a host is not given, assume localhost. use_reloader=False because if not then everything breaks because
+        # the cameras are accessed by OpenCV multiple times which causes the program to get stuck at initializing
+        # the ProcessingEngine
+        self.run(options.get('host', "127.0.0.1"), options.get('port', 3000), options, use_reloader=False)
 
     def route(self, routes):
         """ Registers each URL rule in routes to its specificed endpoint and response function. """
         for url, func in routes.items():
-            self.add_url_rule(url, func.__name__, func, methods=['GET', 'POST'])
+            # if func.__name__ != 'eye':
+                self.add_url_rule(url, func.__name__, func, methods=['GET', 'POST'])
+            # else:
+                # self.add_url_rule('/<CAP_NUM>', func.__name__, func, methods=['GET', 'POST'])
+                # print(self.url_map)
