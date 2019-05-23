b07bfe1ff2e9c3cece5c8f251328e5a4ecfe4b02
duncanmazza <42981118+duncanmazza@users.noreply.github.com>
2019-05-23 18:08:17 -0400

    Fix import issue with cv2.aruco
    There seems to be a bug where the opencv-contrib-python module needs to be uninstalled and then reinstalled for the aruco code to be imported correcty

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-pip install --upgrade -r requirements.txt
-wget -P source/yolo-coco/ https://pjreddie.com/media/files/yolov3.weights
+pip install --upgrade -r requirements.txt;
+pip uninstall opencv-contrib-python;
+pip install opencv-contrib-python;
+wget -P source/yolo-coco/ https://pjreddie.com/media/files/yolov3.weights;
